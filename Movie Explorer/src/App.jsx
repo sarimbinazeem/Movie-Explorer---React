a60170b443652eb55d8ae71e17e96f8fd146d67a
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:42:48 +0500

    Finsiehd app file

--- Movie Explorer/src/App.jsx
+++ Movie Explorer/src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import Search from "./pages/Search";
+import Favorites from "./pages/Favorites";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
 
-export default App
+        <Route path="movies" element={<Movies />} />
+
+        <Route path="movie/:id" element={<MovieDetails />} />
+
+        <Route path="search" element={<Search />} />
+
+        <Route path="favorites" element={<Favorites />} />
+
+        <Route path="about" element={<About />} />
+
+        <Route path="contact" element={<Contact />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
