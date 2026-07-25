84792aeb843bfb668fcce6adbd7166c9b72169e8
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:09:09 +0500

    Added page navigtion

---
+++ Movie Explorer/src/components/PageNavigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PageNavigation = ({page, total, onPrevious, onNext}) => {
+  return (
+    <div>
+        <button onClick={onPrevious} disabled={page===1}>
+            Previous
+
+        </button>
+
+        <p>
+            Page {page} of {total}
+        </p>
+
+        <button onClick={onNext} disabled={page===total}>
+            Next
+        </button>
+    </div>
+  )
+}
+
+export default PageNavigation
