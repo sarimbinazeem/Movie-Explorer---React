adfd716eee7b864b6055738b51ba98cc4117dd3d
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:35:53 +0500

    Added homepage

---
+++ Movie Explorer/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Movie Explorer</h1>
+        <p>Explore your favourite movies</p>
+    </div>
+  )
+}
+
+export default Home
