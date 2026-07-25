53c8d642cfc274ad11f6e8825609ab3cfe76cc8f
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:36:23 +0500

    Added movie page

---
+++ Movie Explorer/src/pages/Movies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Movies = () => {
+  return (
+    <main>
+      <h1>Movies</h1>
+      <p>Browse all available movies.</p>
+    </main>
+  );
+};
+
+export default Movies;
