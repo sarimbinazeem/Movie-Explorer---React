6073598b81573393a1ac4a41552b806ebb99d6ca
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-25 15:22:24 +0500

    Finished Navbar

---
+++ Movie Explorer/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>Movie Explorer</h1>
+
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/favorites">Favorites</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
