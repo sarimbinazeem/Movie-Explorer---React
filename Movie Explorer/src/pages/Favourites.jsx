8bb9808f9a5120cfb54a721a58ddbfef0580d52e
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:38:27 +0500

    Added Favurites Page

---
+++ Movie Explorer/src/pages/Favourites.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <main>
+      <h1>Favorites</h1>
+      <p>Your favorite movies will appear here.</p>
+    </main>
+  );
+};
+
+export default Favorites;
