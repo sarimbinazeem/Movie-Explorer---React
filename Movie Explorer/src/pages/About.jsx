b9730c07e4482703b8c3fb882dc78ef5d46b5283
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-26 00:38:53 +0500

    Added About page

---
+++ Movie Explorer/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>
+        Movie Explorer is a React application that allows users to discover,
+        search, and explore movies using the TMDB API.
+      </p>
+    </main>
+  );
+};
+
+export default About;
