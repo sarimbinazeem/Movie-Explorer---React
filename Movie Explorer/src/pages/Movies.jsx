93500cac6e9a83c0a7311a5b3cc90ffc06cba795
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-28 11:13:36 +0500

    Finished Movies Page

--- Movie Explorer/src/pages/Movies.jsx
+++ Movie Explorer/src/pages/Movies.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import MovieGrid from "../components/MovieGrid";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import { getPopularMovies } from "../services/tmdb";
 
 const Movies = () => {
+  const [movies,setMovies] = useState([])
+  const [loading,setLoading] = useState(true)
+  const [error,setError] = useState("")
+
+  useEffect(()=>{
+    const fetch = async () => {
+      try{
+        const data = await getPopularMovies()
+        setMovies(data)
+      }
+      catch(err)
+      {
+        setError("Unable to fetch movies")
+      }
+      finally{
+        setLoading(false)
+      }
+    }
+
+    fetch()
+  },[])
+
+  if(loading) return <Loading/>
+  if (error) return <ErrorMessage error={error} />;
+
+
   return (
     <main>
-      <h1>Movies</h1>
-      <p>Browse all available movies.</p>
+      <div>
+        <h1>Movies</h1>
+        <p>Browse all the available movies.</p>
+
+      </div>
+
+      <MovieGrid movies={movies} />
     </main>
   );
 };
