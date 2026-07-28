a41f1846e61ba8cab94483fded46c6c08be577fa
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-28 21:34:40 +0500

    Styled Navbar

--- Movie Explorer/src/components/Navbar.jsx
+++ Movie Explorer/src/components/Navbar.jsx
@@ -16,39 +16,50 @@ const Navbar = () => {
     setSearch("");
   };
   return (
-    <nav>
-      <div>
-        <h1>Movie Explorer</h1>
+    <nav className="bg-[#141414] flex justify-between items-center px-12 py-4 border-b border-gray-800 ">
+        <h1 className="text-3xl font-bold text-[#B20710]">MovieNation</h1>
 
-        <form onSubmit={handleSearch}>
-          <input type='text' placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
-          <button type="submit">
+
+        <ul className="flex gap-10 text-gray-300">
+          <li>
+            <NavLink to="/"
+            className={({isActive}) => 
+               isActive? "text-white ease-in duration-300 font-semibold" : "hover:text-white transition duration-300"
+            }>Home</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/movies"             className={({isActive}) => 
+               isActive? "text-white ease-in duration-300 font-semibold" : "hover:text-white transition duration-300"
+            }>Movies</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/favorites"             className={({isActive}) => 
+               isActive? "text-white ease-in duration-300 font-semibold" : "hover:text-white transition duration-300"
+            }>Favorites</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/about"             className={({isActive}) => 
+               isActive? "text-white ease-in duration-300 font-semibold" : "hover:text-white transition duration-300"
+            }>About</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/contact"             className={({isActive}) => 
+               isActive? "text-white ease-in duration-300 font-semibold" : "hover:text-white transition duration-300"
+            }>Contact</NavLink>
+          </li>
+        </ul>
+
+        <form onSubmit={handleSearch} className="flex">
+          <input type='text' placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} 
+          className="px-3 py-2 rounded-l-md bg-gray-800 text-white outline-none"/>
+          <button type="submit" className="bg-red-600 px-4 rounded-r-md hover:bg-red-700 transition font-semibold">
             Search
           </button>
         </form>
-
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/movies">Movies</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/favorites">Favorites</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </div>
     </nav>
   );
 };
