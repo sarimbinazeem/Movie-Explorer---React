42dba32df1b5853a7ed346cf1e9c7eced68dac7e
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-29 00:06:03 +0500

    Styled movie card

--- Movie Explorer/src/components/MovieCard.jsx
+++ Movie Explorer/src/components/MovieCard.jsx
@@ -16,21 +16,24 @@ const MovieCard = ({ id, title, poster, rating, releaseDate }) => {
     setFavourite(status)
   }
   return (
-    <NavLink to={`/movie/${id}`}>
-      <img src={poster} alt={`${title} Poster`} />
+    <div className='bg-[#1c1c1c] rounded-2xl shadow-lg shadow-gray-900 hover:-translate-y-1 p-4 overflow-hidden ease-in duration-300 text-center leading-10 w-full '>
+      <NavLink to={`/movie/${id}`} >
+        <img src={poster} alt={`${title} Poster`}  className="w-full h-80 object-cover "/>
 
-      <h2>{title}</h2>
+        <h2 className="text-xl font-bold line-clamp-2 ">{title}</h2>
 
-      <p>⭐ {rating.toFixed(1)}</p>
+        <p>⭐ {rating ? rating.toFixed(1) : "N/A"}</p>
 
-      <p>{year}</p>
+        <p>{year}</p>
 
-      <button onClick={handleFavourite}>
-        {favorite
-          ? "❤️ Remove Favorite"
-          : "🤍 Add Favorite"}
-      </button>
-    </NavLink>
+      </NavLink>
+
+        <button onClick={handleFavourite} className="bg-[#B20710] rounded-b-2xl py-2 w-full ">
+          {favorite
+            ? "❤️ Remove Favorite"
+            : "🤍 Add Favorite"}
+        </button>
+    </div>
   );
 };
 
