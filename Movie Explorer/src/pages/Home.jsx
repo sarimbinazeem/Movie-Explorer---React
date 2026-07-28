48c67de49c3a83532f14d7200dd032a910f3af30
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-28 11:07:03 +0500

    finished Home Page

--- Movie Explorer/src/pages/Home.jsx
+++ Movie Explorer/src/pages/Home.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MovieGrid from '../components/MovieGrid'
+import Loading from '../components/Loading'
+import { getTrendingMovies,getPopularMovies,getUpcomingMovies,getTopRatedMovies } from '../services/tmdb'
+import Error from '../components/Error'
 
 const Home = () => {
+
+    const [trending, setTrending] = useState([])
+    const [upcoming,setUpcoming] = useState([])
+    const [topRated,setTopRated] = useState([])
+    const [popular,setPopular] = useState([])
+
+    const [loading,setLoading] = useState(true)
+    const [error,setError] = useState("")
+
+    //fetch everytime the homepage renders
+    useEffect(()=>{
+      const fetch = async () =>{
+        try{
+          const [trendingData,popularData,topData, upcomingData] = await Promise.all([
+            getTrendingMovies(),getPopularMovies(),getTopRatedMovies(),getUpcomingMovies()
+          ])
+
+          //Set the fetched data
+          setTrending(trendingData)
+          setPopular(popularData)
+          setTopRated(topData)
+          setUpcoming(upcomingData)
+        }
+        catch (err) 
+        {
+          setError("Failed to load movies.");
+         } 
+         finally {
+         setLoading(false);
+        }
+      }
+      fetch()
+    },[])
+
+    if(loading) return <Loading />
+    if(error) return <Error error={error}/>
+
+
+  
+
   return (
-    <div>
+    <main>
+      <section>
         <h1>Movie Explorer</h1>
         <p>Explore your favourite movies</p>
-    </div>
+
+      </section>
+      <section>
+        <h2>Trending Movies</h2>
+        <MovieGrid movies={trending} />
+      </section>
+
+      <section>
+        <h2>Popular Movies</h2>
+        <MovieGrid movies={popular} />
+      </section>
+
+      <section>
+        <h2>Top Rated Movies</h2>
+        <MovieGrid movies={topRated} />
+      </section>
+
+      <section>
+        <h2>Upcoming Movies</h2>
+        <MovieGrid movies={upcoming} />
+      </section>
+    </main>
   )
 }
 
