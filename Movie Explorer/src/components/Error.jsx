f347180c40176a323e301c8d68e0b1a24e4e859c
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-29 23:46:52 +0500

    Styled Error message

--- Movie Explorer/src/components/Error.jsx
+++ Movie Explorer/src/components/Error.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Error = ({ error }) => {
   return (
-    <div>
-      <h2>Error</h2>
-      <p>{error}</p>
+    <div className="flex flex-col justify-center items-center min-h-[60vh] text-center px-6">
+      <h2 className="text-5xl md:text-6xl font-bold text-[#B20710]">
+        ⚠️ Oops!
+      </h2>
+
+      <h2 className="text-5xl md:text-6xl font-bold text-[#B20710]">
+        Something Went Wrong
+      </h2>
+
+      <p className="mt-2 max-w-xl text-gray-400">
+        {error}
+      </p>
     </div>
   );
 };
