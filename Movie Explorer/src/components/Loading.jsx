be625f26bc65c8b473629da9a0ae5fbd7419a7e6
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-29 19:28:29 +0500

    Styled loading screen

--- Movie Explorer/src/components/Loading.jsx
+++ Movie Explorer/src/components/Loading.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h2>Loading...</h2>
+    <div className="flex justify-center items-center w-full min-h-[90vh] flex-col text-center">
+      <h2 className="text-[#B20710] text-4xl md:text-5xl font-bold animate-pulse">Loading...</h2>
+
+      <p className="mt-4 text-gray-400">
+        Fetching movies...
+      </p>
     </div>
   );
 };
