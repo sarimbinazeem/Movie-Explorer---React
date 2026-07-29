10c8367aeba1a0ecf5f7bde081c76f1cc6273483
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-29 17:54:11 +0500

    Styled genre

--- Movie Explorer/src/components/GenreFilter.jsx
+++ Movie Explorer/src/components/GenreFilter.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const GenreFilter = ({generes,genreItem,setGenreItem}) => {
 
   return (
-    <div>
-        <label>Genre: </label>
+    <div className='bg-[#B20710] px-10 py-5 mt-5 rounded-2xl font-medium shadow-lg shadow-black'>
+        <label className='text-xl font-bold '>Genre: </label>
 
-        <select value={genreItem} onChange={(e)=>setGenreItem(e.target.value)}>
-            <option value=''>All Genres</option>
+        <select value={genreItem} onChange={(e)=>setGenreItem(e.target.value)} className="bg-[#7c040a] px-4 py-2 mt-3 sm:mt-0 sm:ml-5 rounded-xl text-white focus:outline-none focus:ring-2 focus:ring-white cursor-pointer">
+            <option value='' >All Genres</option>
 
             {
                 generes.map((genre)=>{
                     return (
-                    <option key={genre.id} value={genre.id}>
+                    <option key={genre.id} value={genre.id} >
                         {genre.name}
                     </option>
 
